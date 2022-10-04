42c747a0eb3ed053371dbbba1bdb3d2a19a6ab9c
Nicolás Hidalgo <nhidalgos@uft.edu>
2022-10-04 01:03:31 -0300

    Fixeado

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import re
 from pathlib import Path
 from helpers.template import writeTemplate
 
-teamsfolderpath = str(Path('SoccerBots/teams'))
-robocuppath = str(Path('robocup.dsc'))
-jarpath = str(Path('SoccerBots/teams/LabABM.jar'))
-logpath = str(Path('log/log.txt'))
-
 
 def get_files():
     files = []
@@ -15,6 +10,22 @@ def get_files():
         if file.endswith('.java'):
             files.append(file.replace('.java', ''))
     return files
+
+
+teamsfolderpath = str(Path('SoccerBots/teams'))
+robocuppath = str(Path('robocup.dsc'))
+jarpath = str(Path('SoccerBots/teams/LabABM.jar'))
+logpath = str(Path('log/log.txt'))
+
+files = get_files()
+excluded = ["b_And_bb", "b_BehindBall_r", "b_Shorter_vv", "v_SweetSpot_r"]
+files = list(filter(lambda x: x not in excluded, files))
+mapVs = dict()
+mapGoals = dict()
+mapWinners = dict()
+for file in files:
+    mapVs[file] = dict()
+    mapGoals[file] = dict()
 
 
 ## Get combinatories without repetition of 2 elements from a list
@@ -39,13 +50,14 @@ def regex_Text(text):
         return 0, 0
 
 
-def getWinner(mapWin, mapVs, team1, team2):
+def getWinner(mapWin, mapVs, mapGoals, team1, team2):
     ## read file C:\Users\SlimeS\PycharmProjects\LabABM\log.txt
     with open(logpath, "r") as text_file:
         lines = text_file.read()
         g1, g2 = regex_Text(lines)
         if g1 > g2:
             ## check if team1 is in mapWin
+            print(team1, "Ganó", g1,"-", g2)
             if team1 in mapWin:
                 mapWin[team1] += 1
             else:
@@ -54,6 +66,7 @@ def getWinner(mapWin, mapVs, team1, team2):
             mapVs[team2][team1] = -1
         elif g2 > g1:
             ## check if team2 is in mapWin
+            print(team2, "Ganó", g1,"-", g2)
             if team2 in mapWin:
                 mapWin[team2] += 1
             else:
@@ -62,28 +75,44 @@ def getWinner(mapWin, mapVs, team1, team2):
             mapVs[team1][team2] = -1
         else:
             ## draw
-            print("draw")
+            print("Empate", g1,"-", g2)
             mapVs[team1][team2] = 0
             mapVs[team2][team1] = 0
+        mapGoals[team1][team2] = g1
+        mapGoals[team2][team1] = g2
 
 
-files = get_files()
-excluded = ["b_And_bb", "b_BehindBall_r", "b_Shorter_vv", "v_SweetSpot_r"]
-files = list(filter(lambda x: x not in excluded, files))
-mapVs = dict()
-for file in files:
-    mapVs[file] = dict()
+def WriteDualMapCSV(mapToParse, filename):
+    with open(filename, "w") as text_file:
+        # Write as matrix like
+        # ---,team1,team2,team3,team4
+        # team1,0,1,0,0
+        # team2,1,0,0,0
+        # team3,0,0,0,1
+        # team4,0,0,1,0
+        matrix = [["---"] + files]
+        for team1 in files:
+            row = [team1]
+            for team2 in files:
+                if team1 == team2:
+                    row.append("x")
+                else:
+                    row.append(mapToParse[team1][team2])
+            matrix.append(row)
+        for row in matrix:
+            text_file.write(",".join(map(str, row)) + "\n")
+
+
 combinatories = get_combinatories(files)
-mapWinners = dict()
 
 ## execute command: java -jar C:\Users\SlimeS\IdeaProjects\LabABM\Domains\SoccerBots\teams\LabABM.jar C:\Users\SlimeS\IdeaProjects\LabABM\Domains\SoccerBots\teams\robocup.dsc > C:\Users\SlimeS\PycharmProjects\LabABM\log.txt
 
 for comb in combinatories:
     writeTemplate(comb[0], comb[1])
-    print(comb[0], "vs", comb[1])
+    print(comb[0], "vs", comb[1],end=" ->> ")
     os.system(
         f"java -jar {jarpath} {robocuppath} > {logpath}")
-    getWinner(mapWinners, mapVs, comb[0], comb[1])
+    getWinner(mapWinners, mapVs, mapGoals, comb[0], comb[1])
 
     ## write to file
 with open("mapWinners.txt", "w") as text_file:
@@ -92,21 +121,5 @@ with open("mapWinners.txt", "w") as text_file:
         text += f"{key}: {value}\n"
     text_file.write(text)
 
-with open("mapVs.csv", "w") as text_file:
-    # Write as matrix like
-    # ---,team1,team2,team3,team4
-    # team1,0,1,0,0
-    # team2,1,0,0,0
-    # team3,0,0,0,1
-    # team4,0,0,1,0
-    matrix = [["---"] + files]
-    for team1 in files:
-        row = [team1]
-        for team2 in files:
-            if team1 == team2:
-                row.append("x")
-            else:
-                row.append(mapVs[team1][team2])
-        matrix.append(row)
-    for row in matrix:
-        text_file.write(",".join(map(str, row)) + "\n")
+WriteDualMapCSV(mapVs, "mapVs.csv")
+WriteDualMapCSV(mapGoals, "mapGoals.csv")
